5e063a412e7c6ba53e76dd6ba0c0a0f16926655d
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-11-01 18:58:19 -0300

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello Typescript!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(4000, () => {
+  console.log(`Server started on http://localhost:4000/`);
+});
